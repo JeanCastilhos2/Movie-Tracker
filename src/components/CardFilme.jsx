dfd5ebcc67b592eeee672dc7594b97b20e72d65d
Jean Castilhos <jccastilhos182@gmail.com>
2022-08-30 14:07:20 -0300

    subindo para o git-pages

--- src/components/CardFilme.jsx
+++ src/components/CardFilme.jsx
@@ -1,10 +1,98 @@
 import { FaStar } from 'react-icons/fa';
 
+import Styles from './CardFilme.module.css';
+
+import './CardFilme.module.css';
+
 const imageURL = import.meta.env.VITE_IMG;
 
-export function CardFilme({ filme }) {
+export function CardFilme( { filme } ) {
+
+  function cadastrar(){
+
+    const data = {
+    
+      id: filme.id,
+      title: filme.title,
+      poster_path: filme.poster_path,
+      vote_average: filme.vote_average,
+      
+      }
+
+    fetch("http://localhost:4567/addfilme", {
+     method: "POST",
+     headers: {"Content-Type": "application/json", },
+     body: JSON.stringify(data),
+  })
+
+  }
+
+  function deletar(){
+
+    const data = {
+    
+      id: filme.id,
+
+      }
+
+    fetch("http://localhost:4567/delfilme", {
+     method: "POST",
+     headers: {"Content-Type": "application/json", },
+     body: JSON.stringify(data),
+    })
+  }
+
     return (
-        <div>
+   
+
+<div className={Styles.movie_card}>
+  
+    <div className={Styles.container}>
+          
+        <div className={Styles.hero}>
+
+          <img src={imageURL + filme.poster_path} alt={filme.title} />
+            
+            <div className={Styles.details}>
+      
+              <div className={Styles.title1}>{filme.title}<span>{filme.original_language}</span></div>
+
+              <div className={Styles.title2}><FaStar />{filme.vote_average}</div>    
+
+              <div className={Styles.title2}>Duração : {filme.runtime} min</div>  
+
+              <div className={Styles.title2}>Lançamento : {filme.release_date}</div>    
+        
+            </div> 
+      
+        </div> 
+    
+      <div className={Styles.description}>
+      
+            <div className={Styles.column1}>
+            {filme.add === 'true' ?
+             <button className={Styles.btn_add} onClick={deletar}>-</button> 
+              :
+             <button className={Styles.btn_add} onClick={cadastrar}>+</button>
+            } 
+            </div>  
+      
+           <div className={Styles.column2}>
+              <p>{filme.overview}</p>
+           </div> 
+      </div> 
+
+    </div> 
+
+</div> 
+
+
+
+
+      /*   <div>
+
+
+
             <img src={imageURL + filme.poster_path} alt={filme.title} />
             <h1>{filme.title}</h1>
             <p>
@@ -17,7 +105,7 @@ export function CardFilme({ filme }) {
             <p>Bilheteria: U$ {filme.revenue} </p>
             <p>Sinopse:</p>
             <p>{filme.overview}</p>
-        </div>
+        </div>  */
     );
 }
 
--- src/components/CardFilmes.jsx
+++ src/components/CardFilmes.jsx
@@ -5,57 +5,19 @@ import Styles from './CardFilmes.module.css';
 const imageURL = import.meta.env.VITE_IMG;
 
 const CardFilmes = ({ filme }) => {
-
-  function cadastrar(){
-
-    const data = {
-    
-      id: filme.id,
-      title: filme.title,
-      poster_path: filme.poster_path,
-      vote_average: filme.vote_average,
-      
-      }
-
-    fetch("http://localhost:4567/addfilme", {
-     method: "POST",
-     headers: {"Content-Type": "application/json", },
-     body: JSON.stringify(data),
-  })
-
-  }
-
-  function deletar(){
-
-    const data = {
-    
-      id: filme.id,
-
-      }
-
-    fetch("http://localhost:4567/delfilme", {
-     method: "POST",
-     headers: {"Content-Type": "application/json", },
-     body: JSON.stringify(data),
-  })
-
-  }
      
     return(
 
         <div className={Styles.card}>
            <img src={imageURL + filme.poster_path} alt={filme.title} />
-           {filme.add === 'true' ?
-           <button className={Styles.add} onClick={deletar}>-</button> 
-           :
-           <button className={Styles.add_btn} onClick={cadastrar}>+</button>
-           }
            <h2>{filme.title}</h2>
+           <div className={Styles.div}>
            <p>
              {filme.vote_average}<FaStar/>
            </p>
+           </div>
            <Link to={`/filme/${filme.id}`}>
-               <button>Detalhes</button>
+               <button className={Styles.btn_detalhes}>Detalhes</button>
            </Link>        
 
         </div>
